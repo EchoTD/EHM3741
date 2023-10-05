8e190b657329eadeb2a54eb78f98729baab7702a
Can Gursoy <alaaddin.c.gursoy@gmail.com>
2023-10-05 09:46:15 +0300

    first week setuo

---
+++ ex1.py
@@ -0,0 +1,10 @@
+"""
+Kaynak: Intro. to Machine Learning with Python, Andreas C. Müller & Sarah Guido, O'Reilly Media
+
+Değerlendirme Sistemi:  Ödev        | 2 |   %20
+                        Projeler    | 1 |   %30
+                        Ara Sinav   | 1 |   %10
+                        Final       | 1 |   %40
+*Önemli olan yazilan projenin, kodun karşi tarafa satilmasi olayi.
+
+"""
